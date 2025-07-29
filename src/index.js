e3c51081668b854baac5f9b05dd656fce555eef7
EdLaps1w3r <edlaps1w3r@gmail.com>
2025-07-29 13:14:21 +0800

    Fixed errors and added the abiility to remove and tasks to existing lists.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 var listCount = 0;
 var taskCount = 0;
-
+var taskID = 0;
+var tasks = [];
 
 document.getElementById('submit').onclick = function() {
-    const tasks = [];
-    for (let i = 1; i < taskCount; i++) {
-        var eleId = 'task' + i;
-        var task = document.getElementById(eleId).value;
+    var tasks = [];
+    for (let i = 1; i <= taskCount; i++) {
+        var num = taskID + i - taskCount;
+        var eleId = 'task' + num;
+        console.log(eleId);
+        var task = document.getElementById(String(eleId)).value;
         tasks.push(task);
+        if (i !== 1) {
+            const taskInput = document.getElementById(String(eleId));
+            const taskLabel = document.getElementById('label' + num);
+            taskLabel.remove();
+            taskInput.remove();
+        };
     };
     var listName = document.getElementById('title').value;
 
@@ -25,6 +34,11 @@ function closeForm() {
 };
 
 document.getElementById('createListButton').onclick = function() {
+    taskID = taskID + 1;
+    var taskIDTemp = taskID - taskCount;
+    var firstTaskInput = document.getElementById('task' + taskIDTemp);
+    firstTaskInput.id = 'task' + taskID;
+
     var form = document.getElementById('newListForm');
     form.style.display = "block";
     taskCount = 1;
@@ -42,21 +56,24 @@ document.getElementById('close').onclick = function() {
 };
 
 document.getElementById('newFormTask').onclick = function() {
-    taskCount = taskCount++;
+    taskCount = taskCount + 1;
+    taskID = taskID + 1;
     var taskContainer = document.getElementById('taskContainer');
     var newTask = document.createElement('div');
-    var id = 'task' + taskCount;
+    var id = 'task' + taskID;
 
     var label = document.createElement('label');
     label.for = id;
+    label.id = 'label' + taskID;
     label.innerHTML = 'New Task';
     newTask.appendChild(label);
 
     var input = document.createElement('input');
     input.type = 'text';
-    input.placeholder = 'Enter name of new list';
+    input.placeholder = 'Enter New task';
     input.name = id;
-    input.name = id;
+    input.id = id;
+    console.log(input.id);
     newTask.appendChild(input);
     taskContainer.appendChild(newTask);
 };
@@ -80,8 +97,33 @@ function createListDOM(listObj) {
     for (const element of listObj.tasks) {
         var task = document.createElement('li');
         task.innerHTML = element;
+        task.className = 'task';
         tasks.appendChild(task);
     };
 
-    
+    var addTask = tasks.appendChild(document.createElement('li'))
+    var addInput = addTask.appendChild(document.createElement('input'));
+    addInput.type = "text";
+    addInput.placeholder = "Add another Task";
+
+    tasks.addEventListener("click", (e) => {
+        switch (e.target.className) {
+            case "task":
+                e.target.remove();
+                break;
+            default:
+                break;
+        };
+    });
+
+    addInput.addEventListener("keydown", (e) => {
+        if (e.key === 'Enter') {
+            var task = document.createElement('li')
+            task.innerHTML = e.currentTarget.value;
+            e.currentTarget.before(task);
+            taskID = taskID + 1;
+            e.currentTarget.value = '';
+        };
+    });
 };
+
